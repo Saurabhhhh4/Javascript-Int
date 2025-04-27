dbdb2aed65ab2abc51c3fbe729c41e7e2bdc6b99
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-04-27 17:13:05 +0530

    socket

--- ques.js
+++ ques.js
@@ -15,15 +15,57 @@
 // }, []);
 // console.log(uniqueArray); // [1, 2, 3, 4, 5]
 
-function removeDuplicates(array) {
-  var unique = [];
-  for (var i = 0; i < array.length; i++) {
-    if (unique.indexOf(array[i]) === -1) {
-      unique.push(array[i]);
-    }
-  }
-  return unique;
-}
+// function removeDuplicates(array) {
+//   var unique = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (unique.indexOf(array[i]) === -1) {
+//       unique.push(array[i]);
+//     }
+//   }
+//   return unique;
+// }
 
-var array = [1, 2, 2, 3, 4, 4, 5];
-console.log(removeDuplicates(array)); // [1, 2, 3, 4, 5]
+// var array = [1, 2, 2, 3, 4, 4, 5];
+// console.log(removeDuplicates(array)); // [1, 2, 3, 4, 5]
+const http = require("http");
+const socketIo = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+  socket.on("disconnect", () => console.log("User disconnected"));
+});
+
+server.listen(3000, () => console.log("API Gateway running on port 3000"));
+
+const CircuitBreaker = require("opossum");
+
+const serviceCall = () => axios.get("http://localhost:4000/users");
+
+const breaker = new CircuitBreaker(serviceCall, {
+  timeout: 3000,
+  errorThresholdPercentage: 50,
+  resetTimeout: 10000,
+});
+
+breaker.fallback(() => "Service is currently unavailable");
+
+breaker
+  .fire()
+  .then((response) => console.log(response.data))
+  .catch((error) => console.log(error.message));
+const jwt = require("jsonwebtoken");
+
+app.use((req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) return res.status(403).send("Access denied.");
+
+  jwt.verify(token, "secretKey", (err, decoded) => {
+    if (err) return res.status(401).send("Unauthorized");
+    req.user = decoded;
+    next();
+  });
+});
