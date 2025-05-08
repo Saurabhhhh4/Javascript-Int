53d660191c72f7269c1dcadc46f785ba1b1129a7
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-05-08 08:33:23 +0530

    map

--- setmap.js
+++ setmap.js
@@ -204,11 +204,159 @@ const num = [1, 2, 2, 3, 4, 4, 5];
 // const myArray = Array.from(mySet);
 // console.log(myArray); // [1, 3, 4]
 
-const set1 = new Set([1, 2, 3, 4]);
-const set2 = new Set([4, 6, 7, 8]);
+// const set1 = new Set([1, 2, 3, 4]);
+// const set2 = new Set([4, 6, 7, 8]);
 
 // const combine = new Set([...set1, ...set2]);
 // console.log(combine);
 
-const intersection = new Set([...set1].filter((x) => set2.has(x)));
-console.log(intersection);
+// const intersection = new Set([...set1].filter((x) => set2.has(x)));
+// console.log(intersection);
+
+// function intersection(set1, set2) {
+//   //   return new Set([...set1].filter((x) => set2.has(x)));
+//   return [...set1].filter((x) => set2.has(x));
+// }
+// const set1 = new Set([1, 2, 3, 4]);
+// const set2 = new Set([4, 6, 7, 8]);
+// const result = intersection(set1, set2);
+
+// console.log(result);
+
+// const sentence = "helllo baby what do you do";
+
+// const uniq = [...new Set(sentence.split(" "))];
+// console.log(uniq);
+
+// const developers = [
+//   { name: "Alice", skills: ["JavaScript", "React"] },
+//   { name: "Bob", skills: ["Node.js", "JavaScript"] },
+//   { name: "Charlie", skills: ["React", "TypeScript"] },
+// ];
+
+// const allskill = developers.flatMap((dev) => dev.skills);
+
+// const uniqskill = [...new Set(allskill)];
+// console.log(uniqskill);
+///
+
+// const matrix = [
+//   [1, 2, 3],
+//   [2, 3, 4],
+//   [4, 5, 6],
+// ];
+
+// const flatArray = matrix.flat();
+
+// const uniq = [...new Set(flatArray)];
+
+// console.log(uniq);
+
+// const users = [
+//   { id: 1, name: "Alice" },
+//   { id: 2, name: "Bob" },
+//   { id: 1, name: "Alice" }, // duplicate
+//   { id: 3, name: "Charlie" },
+// ];
+
+// const seen = new Set();
+
+// const uniq = users.filter((user) => {
+//   if (seen.has(user.id)) {
+//     return false;
+//   } else {
+//     seen.add(user.id);
+//     // return true;
+//   }
+// });
+// console.log(uniq);
+
+// const input = "abacjhsfdgjfsjfsss";
+
+// const x = new Set();
+
+// let repeat = null;
+
+// for (let char of input) {
+//   if (x.has(char)) {
+//     repeat = char;
+//     break;
+//   }
+//   x.add(char);
+// }
+// console.log(repeat);
+
+// const sentence = "apple banana apple banana chips chips randon";
+
+// const words = sentence.split(" ");
+
+// let x = new Map();
+
+// for (let word of words) {
+//   x.set(word, (x.get(word) || 0) + 1);
+// }
+// const result = Object.fromEntries(x);
+// console.log(result);
+
+// const numbers = [1, 2, 4, 6, 7, 9];
+
+// const start = 1;
+// const end = 10;
+
+// const numSet = new Set(numbers);
+
+// const miss = [];
+
+// for (let i = start; i <= end; i++) {
+//   if (!numSet.has(i)) {
+//     miss.push(i);
+//   }
+// }
+// console.log(miss);
+
+// let a = 1,
+//   b = 2;
+
+// let temp = a;
+// a = b;
+// b = temp;
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [3, 4, 5, 6, 7];
+
+// const set2 = new Set(arr2);
+
+// const common = arr1.filter((item) => set2.has(item));
+
+// console.log(common);
+
+// const a = [1, 2, 3, 4];
+// const b = [3, 4, 5, 6];
+
+// const setB = new Set(b);
+
+// const diff = a.filter((item) => !setB.has(item));
+
+// console.log(diff);
+
+const items = [
+  { name: "apple", category: "fruit" },
+  { name: "banana", category: "fruit" },
+  { name: "carrot", category: "vegetable" },
+  { name: "broccoli", category: "vegetable" },
+];
+
+const group = new Map();
+
+for (let item of items) {
+  const key = item.category;
+
+  if (!group.has(key)) {
+    group.set(key, []);
+  }
+  group.get(key).push(item.name);
+}
+console.log(group);
