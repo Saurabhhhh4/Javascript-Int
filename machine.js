40097a95699687e68f2b6633ee6b3447d83c272f
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-06-07 19:36:06 +0530

    fact

--- machine.js
+++ machine.js
@@ -1,7 +1,68 @@
-function sum(a, b) {
-  return a + b;
+// Write a JavaScript function that calculates the sum of two numbers
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// const num1 = 10;
+// const num2 = 20;
+// const result = sum(num1, num2);
+// console.log("The sum of", num1, "and", num2, "is", result);
+
+///////////////////////////////
+// Write a JavaScript program to reverse the given string
+
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+// const input = "saurabh the goat ";
+// const reversedString = reverseString(input);
+// console.log("Reversed String :", reversedString);
+
+// Write a JavaScript function that finds the maximum number in an array
+// function findMax(arr) {
+//   if (arr.length === 0) {
+//     return null; // Handle empty array
+//   }
+//   let max = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+// const numbers = [10, 5, 25, 8, 15];
+// const maxNumber = findMax(numbers);
+// console.log("The maximum number is:", maxNumber);
+
+// function maxNum(arr) {
+//   if (arr.length === 0) {
+//     return null;
+//   }
+//   let max = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// const num = [10, 20, 93, 20];
+// const maxNumm = maxNum(num);
+// console.log(maxNumm);
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-const num1 = 10;
-const num2 = 20;
-const result = sum(num1, num2);
-console.log("The sum of", num1, "and", num2, "is", result);
+const number = 5;
+const result = factorial(number);
+console.log("Factorial of", number, "is", result);
