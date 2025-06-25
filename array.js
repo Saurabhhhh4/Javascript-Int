9fcd0c760d07d152f54aa2e6f4aefe6608622e45
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-06-25 22:52:05 +0530

    first

--- array.js
+++ array.js
@@ -1,12 +1,32 @@
 // first letter of every word should be in upper case
 
-function capitalizeFirstLetterOfEachWord(str) {
-  const words = str.split(" ");
-  const capitalizedWords = words.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  });
-  return capitalizedWords.join(" ");
+// function capitalizeFirstLetterOfEachWord(str) {
+//   const words = str.split(" ");
+//   const capitalizedWords = words.map((word) => {
+//     return word.charAt(0).toUpperCase() + word.slice(1);
+//   });
+//   return capitalizedWords.join(" ");
+// }
+// const inputString = "hello world this is a sample string";
+// const capitalizedString = capitalizeFirstLetterOfEachWord(inputString);
+// console.log("Capitalized string:", capitalizedString);
+
+// ......AbortController
+
+function findLargestElement(arr) {
+  let max = -Infinity;
+  function helper(nestedArr) {
+    for (let item of nestedArr) {
+      if (typeof item === "number") {
+        max = Math.max(max, item);
+      } else if (Array.isArray(item)) {
+        helper(item);
+      }
+    }
+  }
+  helper(arr);
+  return max;
 }
-const inputString = "hello world this is a sample string";
-const capitalizedString = capitalizeFirstLetterOfEachWord(inputString);
-console.log("Capitalized string:", capitalizedString);
+const nestedArray = [1, [2, 3, [4, 5]], 6, [7, 8]];
+const largestElement = findLargestElement(nestedArray);
+console.log("Largest element:", largestElement);
