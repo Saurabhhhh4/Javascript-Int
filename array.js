5ca38852f764e3fc7504070124c8b15c281aa4e1
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-07-01 23:04:20 +0530

    change

--- array.js
+++ array.js
@@ -53,12 +53,13 @@ function findMax(arr) {
   if (arr.length === 0) {
     return null;
   }
-  let max = arr[1];
+  let max = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
     }
   }
+  return max;
 }
 
 const arr = [3, 6, 8, 4, 9];
