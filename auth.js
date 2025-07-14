ac40065430cb9969867dd5c14f21897326928ca5
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-07-14 21:47:37 +0530

    rest

--- auth.js
+++ auth.js
@@ -21,20 +21,19 @@ exports.login = async (req, res) => {
   );
   res.json({ token });
 };
-\\
+// \\
 // json BaseAudioContext
 
-const jwt = require('jsonwebtoken');
-module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+// const jwt = require('jsonwebtoken');
+// module.exports = (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Now available in all protected routes
-    next();
-  } catch {
-    res.status(401).json({ message: "Invalid Token" });
-  }
-};
-
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded; // Now available in all protected routes
+//     next();
+//   } catch {
+//     res.status(401).json({ message: "Invalid Token" });
+//   }
+// };
