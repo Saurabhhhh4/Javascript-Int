34b76c9a3462895a2ef937bc5aeb0bc99b66afff
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-07-15 23:35:56 +0530

    arr

--- array.js
+++ array.js
@@ -114,3 +114,11 @@ function flattenArray(arr) {
 const nestedArray = [1, [2, 3], 4, [5, 6, [7, 8]]];
 const flattenedArray = flattenArray(nestedArray);
 console.log("Flattened array:", flattenedArray);
+
+let arr = [1, 2, 3, 4, 5];
+let reversedArr = [...arr].reverse(); // or arr.slice().reverse()
+console.log(reversedArr); // [5, 4, 3, 2, 1]
+
+let arr1 = [10, 20, 30, 40, 50];
+arr.reverse();
+console.log(arr);
