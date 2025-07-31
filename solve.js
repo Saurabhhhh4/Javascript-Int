8b4ddb0326e413b9597025df79bdf7ccfd0c845f
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-08-01 00:52:00 +0530

    dictonary

--- solve.js
+++ solve.js
@@ -119,6 +119,28 @@
 // ];
 // console.log(secondHighest(arr)); // Output: "a"
 
+// function wordBreak(s, wordDict) {
+//   const wordSet = new Set(wordDict);
+//   const dp = Array(s.length + 1).fill(false);
+//   dp[0] = true;
+
+//   for (let i = 1; i <= s.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (dp[j] && wordSet.has(s.substring(j, i))) {
+//         dp[i] = true;
+//         break;
+//       }
+//     }
+//   }
+
+//   return dp[s.length];
+// }
+
+// // Test case
+// const s = "applepenapple";
+// const wordDict = ["apple", "pen"];
+// console.log(wordBreak(s, wordDict)); // ✅ Output: true
+
 function wordBreak(s, wordDict) {
   const wordSet = new Set(wordDict);
   const dp = Array(s.length + 1).fill(false);
